5b33354f49d796d808710794c38f736ef9e7730b
hoon <nhoon95@gmail.com>
2020-03-13 21:07:48 +0900

    두번째

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use("/", userRouter);
+
+export default app;
